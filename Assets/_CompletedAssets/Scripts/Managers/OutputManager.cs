478a16f6e6c684e62f1fd429f103a976d1c3fbb8
Raid <ralbaya1@asu.edu>
2016-02-19 20:11:49 -0700

    Initial commit :tada:

---
+++ Assets/_CompletedAssets/Scripts/Managers/OutputManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using TCP;
+using System.Collections;
+
+namespace CompleteProject
+{
+
+    public class OutputManager : MonoBehaviour
+    {
+        public testLib tcp = new testLib();
+
+        // Use this for initialization
+        void Awake()
+        {
+            tcp.StartClient();
+
+        }
+
+        public testLib tcpReturn()
+        {
+            return tcp;
+        }
+
+    }
+}
---
+++ Assets/_CompletedAssets/Scripts/Managers/WriteFile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+
+namespace CompleteProject
+{
+    public class WriteFile : MonoBehaviour
+    {
+        public String fileN = "Output.txt";
+
+        //void Start()
+        //{
+        //    File.AppendAllText(fileN, "New Game" + Environment.NewLine);
+        //}
+
+        public void WriteToFile(String code)
+        {
+            String output = (ToUnixTime()).ToString();
+            output += "," + code + Environment.NewLine;
+
+            File.AppendAllText(fileN, output);
+
+            if (code.Equals("Player Died"))
+            {
+                File.AppendAllText(fileN, "Game End" + Environment.NewLine);
+                File.AppendAllText(fileN, Environment.NewLine);
+            }
+        }
+
+        // convert datetime to unix epoch seconds
+        private static long ToUnixTime()
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return Convert.ToInt64((DateTime.Now.ToUniversalTime() - epoch).TotalSeconds);
+        }
+
+    }
+}
